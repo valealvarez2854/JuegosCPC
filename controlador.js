640af574fc99b0de4c18100881404890b871b95a
valealvarez2854 <61520291+valealvarez2854@users.noreply.github.com>
2021-09-15 16:56:26 -0500

    HOLI

--- controlador.js
+++ controlador.js
@@ -1,6 +1,8 @@
 let calcular=document.getElementById("calcular");
+let siguiente=document.getElementById("siguiente");
 
 calcular.addEventListener("click",verificarRespuesta);
+siguiente.addEventListener("click",cambiarDatos);
 
 function verificarRespuesta(){
     
@@ -9,23 +11,39 @@ function verificarRespuesta(){
     let numero2=document.getElementById("numero2").value;
     let resultado=document.getElementById("resultado").value;
     let gift=document.getElementById("gift");
+    
 
     let calcularResultado= Number(numero1)+Number(numero2);
     
+   
+
+    
+    
     if(resultado==calcularResultado){
 
-        console.log("ok");
+        
         gift.classList.remove("invisible");
-        gift.src="https://giphy.com/embed/3o7abKhOpu0NwenH3O";
+        gift.src="https://giphy.com/embed/3oEjHV0z8S7WM4MwnK";
 
     }else{
-        console.log("ojo");
+        
         gift.classList.remove("invisible");
-        gift.src="https://giphy.com/embed/xU9TT471DTGJq";
+        gift.src="https://giphy.com/embed/11NQsJmv6mJzag";
 
     }
 
-
-
+    
 
 }
+
+function cambiarDatos(){
+    gift.classList.remove("visible");
+    numero1.value=getRandomInt(1,10);
+    numero2.value=getRandomInt(1,10);
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+      
+     
+}
+
